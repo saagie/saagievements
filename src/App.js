0f54086a5a13450f609aa649b717e8cba4e94a06
Jérémie Zarca <jeremie.zarca@gmail.com>
2020-07-07 15:37:23 +0200

    used hooks + updated manifest

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Achievements } from './pages/Achievements';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Achievements } from "./pages/Achievements";
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Achievements />
-          </Route>
-          {/* <Route exact path="/new-achievement">
+export default () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <Achievements />
+      </Route>
+      {/* <Route exact path="/new-achievement">
             <NewAchievement />
           </Route> */}
-        </Switch>
-      </BrowserRouter>
-    );
-  }
-}
-
-export default App;
+    </Switch>
+  </BrowserRouter>
+);
--- src/pages/Achievements.js
+++ src/pages/Achievements.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import {
   AppLayoutPage,
   Datalist,
@@ -8,60 +8,49 @@ import {
   DatalistColActions,
   PageHeader,
   Icon,
-} from 'saagie-ui/react';
+} from "saagie-ui/react";
 
-export class Achievements extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      achievements: [],
-    };
-  }
+export const Achievements = () => {
+  const [achievements, setAchievements] = useState([]);
 
-  componentDidMount() {
-    fetch('/api/achievements')
+  useEffect(() => {
+    fetch("/api/achievements")
       .then((body) => body.json())
       .then((achievements) => {
-        this.setState({
-          achievements,
-        });
+        setAchievements(achievements);
       });
-  }
+  }, []);
 
-  render() {
-    const {
-      achievements,
-    } = this.state;
-
-    return (
-      <div className="sui-l-app-layout">
-        <div className="sui-l-app-layout__subapp">
-          <AppLayoutPage>
-            <PageHeader title="SaagieVements">
-              <Link to="/new-achievement" className="sui-a-button as--primary">
-                New achievement
-              </Link>
-            </PageHeader>
-            <h3>
-              Have fun to unlock the following achievements
-            </h3>
-            <Datalist isHover>
-              {achievements.map((achievement) => (
-                <DatalistRow
-                  key={achievement.goal}
-                >
-                  <DatalistCol isLink level="primary">
-                    {achievement.goal}
-                  </DatalistCol>
-                  <DatalistColActions size="xs">
-                    <Icon style={{ opacity: achievement.unlocked ? 1 : 0.4 }} name={achievement.unlocked ? 'fa-trophy' : 'fa-times-circle'} size="xl" />
-                  </DatalistColActions>
-                </DatalistRow>
-              ))}
-            </Datalist>
-          </AppLayoutPage>
-        </div>
+  return (
+    <div className="sui-l-app-layout">
+      <div className="sui-l-app-layout__subapp">
+        <AppLayoutPage>
+          <PageHeader title="SaagieVements">
+            <Link to="/new-achievement" className="sui-a-button as--primary">
+              New achievement
+            </Link>
+          </PageHeader>
+          <h3>Have fun to unlock the following achievements</h3>
+          <Datalist isHover>
+            {achievements.map((achievement) => (
+              <DatalistRow key={achievement.goal}>
+                <DatalistCol isLink level="primary">
+                  {achievement.goal}
+                </DatalistCol>
+                <DatalistColActions size="xs">
+                  <Icon
+                    style={{ opacity: achievement.unlocked ? 1 : 0.4 }}
+                    name={
+                      achievement.unlocked ? "fa-trophy" : "fa-times-circle"
+                    }
+                    size="xl"
+                  />
+                </DatalistColActions>
+              </DatalistRow>
+            ))}
+          </Datalist>
+        </AppLayoutPage>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
